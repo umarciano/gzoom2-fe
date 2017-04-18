228c9d01e025d6d30186f1c8d822dc014976eb5a
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2017-04-18 16:29:02 +0200

    Style fixed

--- app/src/app/layouts/focus/focus.component.ts
+++ app/src/app/layouts/focus/focus.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-focus',
 	templateUrl: './focus.component.html',
-	styleUrls: ['./focus.component.css']
+	styleUrls: ['./focus.component.scss']
 })
 export class FocusComponent { }
