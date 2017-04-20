6e11c8f8ccfc5c1e8164046a5bd3804af190df78
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2017-04-20 17:16:40 +0200

    Fallback to /dashboard if / is hitten

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // NOTE this is a sort of hack that will help us redirect to /dashboard
+    // if / url is hitten; seems like there's no way to do this
+    if (this.router.url === '/') {
+      this.router.navigate(['/dashboard']);
+    }
+  }
+}
