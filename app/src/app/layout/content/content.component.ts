e574dcc33f67290240301bf3b77c53366aabd02d
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2017-04-21 19:01:22 +0200

    Lints solved

--- app/src/app/layout/content/content.component.ts
+++ app/src/app/layout/content/content.component.ts
@@ -5,14 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent {
-  // TODO make these properties connected to sidebar
-  activeMenuId: string;
-  mobileMenuActive: boolean = false;
-
-  toggleMenu(e) {
-    this.mobileMenuActive = !this.mobileMenuActive;
-    e.preventDefault();
-  }
-
-}
+export class ContentComponent { }
