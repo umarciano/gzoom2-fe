f95952f68c3dfab8d62c596a863aaad4f7a7c552
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2017-04-28 16:59:34 +0200

    Comments++

--- app/src/app/api/logout.service.ts
+++ app/src/app/api/logout.service.ts
@@ -5,10 +5,19 @@ import 'rxjs/add/operator/toPromise';
 
 const LOGOUT_ENDPOINT = 'logout';
 
+/**
+ * Allows user to log out.
+ */
 @Injectable()
 export class LogoutService {
   constructor(private readonly client: ApiClientService) { }
 
+  /**
+   * Logs user out. This method is never supposed to fail even if calls to backend
+   * service returns an error.
+   *
+   * @return {Promise<any>} A promise of any result that always succeeds.
+   */
   logout(): Promise<any> {
     return this.client
       .post(LOGOUT_ENDPOINT)
