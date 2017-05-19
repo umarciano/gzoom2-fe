27e11d4f11d45b420231744b99d8ead9694d52d6
Fabio Strozzi <fabio.strozzi@mapsgroup.it>
2017-05-19 12:40:39 +0200

    Safe ID guard fixed
    Regular expressions do not have to be compiled anymore in Javascript!

--- app/src/app/shared/safe-identifier.guard.ts
+++ app/src/app/shared/safe-identifier.guard.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+const ID_REGEXP = /^[0-9a-zA-Z\._\-]+$/;
+
+/**
+ * Creates a safety guard that prevents routes to be loaded when the id parameter does not
+ * match the safe-id regular expression.
+ * Such ids cannot contain paranthesis and colons; the only valid characters are digits,
+ * alphabetic ascii, dots, dashes and underscores.
+ */
 @Injectable()
 export class SafeIdentifierGuard implements CanActivate {
-  private readonly regExp = /^[0-9a-zA-Z\.\-]+$/.compile();
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const id = next.params['id'] as string;
-    return id && this.isSafe(id);
-  }
-
-  isSafe(id: string) {
-    return this.regExp.test(id);
+    return id && ID_REGEXP.test(id);
   }
 }
