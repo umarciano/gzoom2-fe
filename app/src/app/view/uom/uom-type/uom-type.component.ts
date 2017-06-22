a0394ad65980f59968fabd99f95714812089e1a7
Daina Interrante <daina.interrante@mapsgroup.it>
2017-06-22 10:40:03 +0200

    uom-type sortorder

--- app/src/app/view/uom/uom-type/uom-type.component.ts
+++ app/src/app/view/uom/uom-type/uom-type.component.ts
@@ -17,49 +17,30 @@ export class UomTypeComponent implements OnInit {
   uomTypes2: UomType[];
   cols: any[];
 
-  datasource: UomType[];
-  cars: UomType[];
-  totalRecords: number;
-
   constructor(private uomService: UomService, private router: Router) { }
 
   ngOnInit() {
     console.log('ngOnInit');
-    this.uomTypes = Observable.of<UomType[]>([]);
-    this.uomService.search()
+    // this.uomTypes = Observable.of<UomType[]>([]);
+    this.uomService.uomType()
     .toPromise()
-    .then(cars => {
-      // this.uomTypes2 = cars
-      this.datasource = cars;
-      this.totalRecords = this.datasource.length;
-      this.cars = this.datasource.slice(0, 3);
+    .then(uomTypes => {
+      this.uomTypes2 = uomTypes
     })
     .catch(error => {
-        // TODO: add real error handling
-        console.log(error);
-        return Observable.of<UomType[]>([]);
-      });
+      // TODO: add real error handling
+      console.log(error);
+      return Observable.of<UomType[]>([]);
+    });
 
-      this.cols = [
-          {field: 'uomTypeId', header: 'UomTypeId'},
-          {field: 'description', header: 'Description'}
-      ];
-    }
+    this.cols = [
+      {field: 'uomTypeId', header: 'UomTypeId'},
+      {field: 'description', header: 'Description'}
+    ];
+  }
 
-    loadCarsLazy(event: LazyLoadEvent) {
-        //in a real application, make a remote request to load data using state metadata from event
-        //event.first = First row offset
-        //event.rows = Number of rows per page
-        //event.sortField = Field name to sort with
-        //event.sortOrder = Sort order as number, 1 for asc and -1 for dec
-        //filters: FilterMetadata object having field as key and filter value, filter matchMode as value
-
-        //imitate db connection over a network
-        setTimeout(() => {
-            if(this.datasource) {
-                this.cars = this.datasource.slice(event.first, (event.first + event.rows));
-            }
-    }, 250);
-
+  selectUomType(uomType: UomType) {
+    // this.msgs = [];
+    // this.msgs.push({severity:'info', summary:'Car Select', detail:'Vin: ' + car.vin});
   }
 }
