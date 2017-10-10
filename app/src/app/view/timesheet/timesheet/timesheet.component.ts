cabd9548453c8ac0bcb05683a51792eb56a7afb3
Andrea Liguori <andrea.liguori@mapsgroup.it>
2017-10-10 10:33:06 +0200

    GN-2407 Nuova funzione Dati Anagrafici di Periodo

--- app/src/app/view/timesheet/timesheet/timesheet.component.ts
+++ app/src/app/view/timesheet/timesheet/timesheet.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+
+import {Timesheet} from './timesheet';
 
 @Component({
   selector: 'app-timesheet',
@@ -7,9 +14,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimesheetComponent implements OnInit {
 
-  constructor() { }
+  timesheets: Timesheet[];
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router) {
+  }
 
   ngOnInit() {
+    this.route.data
+      .map((data: { timesheets: Timesheet[] }) => data.timesheets)
+      .subscribe(data => this.timesheets = data);
   }
 
 }
