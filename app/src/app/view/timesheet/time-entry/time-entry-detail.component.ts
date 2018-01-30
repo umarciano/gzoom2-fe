83b294741a48abf4f69461584bea72125fe1516b
Andrea Liguori <andrea.liguori@mapsgroup.it>
2018-01-30 12:59:01 +0100

    GN-2470

--- app/src/app/view/timesheet/time-entry/time-entry-detail.component.ts
+++ app/src/app/view/timesheet/time-entry/time-entry-detail.component.ts
@@ -22,6 +22,7 @@ import { templateJitUrl } from '@angular/compiler';
 
 import {InputTextModule} from 'primeng/primeng';
 import {AutoCompleteModule} from 'primeng/primeng';
+import { isBlank } from 'app/commons/commons';
 
 
 @Component({
@@ -44,6 +45,8 @@ export class TimeEntryDetailComponent implements OnInit {
   selectedTimesheetId: string;
   selectedWorkEffortId: string;
   workEffortSelectItem: SelectItem[] = [];
+  activities: SelectItem[] = [];
+  filteredActivities: any[];
 
   constructor(
     private readonly timesheetService: TimesheetService,
@@ -58,10 +61,6 @@ export class TimeEntryDetailComponent implements OnInit {
 
   ngOnInit() {
     console.log(" - ngOnInit ");
-
-    // this.form = this.fb.group({
-    //   'workEffortId': new FormControl('', Validators.required)
-     // });
 
     const reloadedWorkEffort = this._reload.switchMap(() => this.timesheetService.workEfforts(this.selectedTimesheetId));
     const reloadedTimeEntries = this._reload.switchMap(() => this.timesheetService.timeEntries(this.selectedTimesheetId));
@@ -151,6 +150,16 @@ export class TimeEntryDetailComponent implements OnInit {
         this.error = this.i18nService.translate(error.message) || error;
       });
   }
+
+  filterActivities(event) {
+    this.filteredActivities = [];
+     for(let i = 0; i < this.workEffortSelectItem.length; i++) {
+         let record = this.workEffortSelectItem[i];
+         if(record.label.toLowerCase().indexOf(event.query.toLowerCase()) == 0) {
+                     this.filteredActivities.push(record.label);
+          }
+     }
+  }
 }
 
 function workEFforts2SelectItems(workEffort: WorkEffort[]): SelectItem[] {
@@ -163,5 +172,3 @@ class PrimeTimeEntry implements TimeEntry {
   constructor(public dirty:boolean, public timeEntryId?: string, public timesheetId?: string, public workEffortId?: string,
               public description?: string, public percentage?: number) { }
 }
-
-
