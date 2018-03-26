f58c53faa6888b2b34121a1795b22ace51289e94
Daina Interrante <daina.interrante@mapsgroup.it>
2018-03-26 18:16:17 +0200

    fix for build

--- app/src/app/view/timesheet/timesheet-routing.module.ts
+++ app/src/app/view/timesheet/timesheet-routing.module.ts
@@ -7,7 +7,6 @@ import { TimesheetResolver } from './timesheet/timesheet-resolver.service';
 import { TimesheetComponent } from './timesheet/timesheet.component';
 import { TimeEntryResolver } from './time-entry/time-entry-resolver.service';
 import { WorkEffortResolver } from './time-entry/work-effort-resolver.service';
-import { TimeEntryComponent } from './time-entry/time-entry.component';
 import { TimeEntryDetailComponent } from './time-entry/time-entry-detail.component';
 
 import { PartyResolver } from '../party/party/party-resolver.service';
