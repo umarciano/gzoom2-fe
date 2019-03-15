73bc3152134cd4993fd8c92b4f050f0f9a1ef5f4
Assuntina <assuntina.magnante@mapsgroup.it>
2019-03-15 10:09:04 +0100

    2.3.2rc1

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -44,7 +44,7 @@ const GZOOM_PATH = '/gzoom/control/box';
     // application modules
     // > libraries
     CommonsModule.forRoot({
-      application: { name: 'GZoom', version: '2.3.1rc1' },
+      application: { name: 'GZoom', version: '2.3.2rc1' },
       i18n: { rootPath: ROOT_PATH }
     }),
     ApiModule.forRoot({ rootPath: ROOT_PATH, gzoomPath: GZOOM_PATH }),
