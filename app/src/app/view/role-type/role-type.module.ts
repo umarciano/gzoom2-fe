42fff15785934da35d7d27fc0fd9fd464d20734c
Assuntina <assuntina.magnante@mapsgroup.it>
2019-03-15 10:59:34 +0100

    bug-fix

--- app/src/app/view/role-type/role-type.module.ts
+++ app/src/app/view/role-type/role-type.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RoleTypeComponent } from './role-type/role-type.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [RoleTypeComponent]
 })
 export class RoleTypeModule { }
--- app/src/app/view/role-type/role-type/role-type.component.ts
+++ app/src/app/view/role-type/role-type/role-type.component.ts
@@ -10,6 +10,7 @@ export class RoleTypeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit');
   }
 
 }
