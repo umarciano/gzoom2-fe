2c993107cd63f8aabaff904696cfa602129960ed
Antonio Calò <antonio.calo@mapsgroup.it>
2020-10-27 17:25:39 +0100

    release version 2.6.8

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -46,7 +46,7 @@ const GZOOM_PATH = '/gzoom/control/box';
     // application modules
     // > libraries
     CommonsModule.forRoot({
-      application: { name: 'GZoom2', version: '2.6.7'}
+      application: { name: 'GZoom2', version: '2.6.8'}
       //, i18n: { rootPath: ROOT_PATH }
     }),
     ApiModule.forRoot({ rootPath: ROOT_PATH, gzoomPath: GZOOM_PATH }),
